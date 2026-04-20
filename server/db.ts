7b4bc77df87952b008298099dfe343b78988c74a
jawad35 <jaqimughal@gmail.com>
2026-04-20 16:42:23 +0500

    database url change

--- server/db.ts
+++ server/db.ts
@@ -1,7 +1,7 @@
 import { drizzle } from "drizzle-orm/node-postgres";
 import { Pool } from "pg";
 
-const connectionString = 'postgresql://postgres:1234@localhost:5432/postest';
+const connectionString = 'postgresql://pos_system_oehj_user:GlM4As656wmZZqKdndchJfB28AS0ZRe7@dpg-d7j0vj8sfn5c73ee266g-a.ohio-postgres.render.com/pos_system_oehj';
 
 const pool = new Pool({
   connectionString,
